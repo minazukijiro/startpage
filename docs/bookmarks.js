caef7cc8ba3ed03947c4075723dba978a34af2b8
ﾀﾅｶ ﾄﾓｱｷ <minazukijiro@gmail.com>
2024-09-25 20:14:44 +0900

    Update prime video url

--- docs/bookmarks.js
+++ docs/bookmarks.js
@@ -1,36 +1,36 @@
 // Note: having length != 4 will mess with layout based on how the site is styled
 const bookmarks = [
-    {
-        title: "Daily",
-        links: [
-            { name: "Gmail",    url: "https://mail.google.com" },
-            { name: "Calendar", url: "https://calendar.google.com" },
-            { name: "GitHub",   url: "https://github.com" },
-        ],
-    },
-    {
-        title: "Media",
-        links: [
-            { name: "Youtube",     url: "https://youtube.com" },
-            { name: "Disney+",     url: "https://www.disneyplus.com" },
-            { name: "Prime Video", url: "https://www.amazon.com/Amazon-Video" },
-        ],
-    },
-    {
-        title: "Reddit",
-        links: [
-            { name: "r/commandline", url: "https://www.reddit.com/r/commandline" },
-            { name: "r/unixporn",    url: "https://www.reddit.com/r/unixporn" },
-        ],
-    },
-    {
-        title: "Shopping",
-        links: [
-            { name: "Amazon",        url: "https://amazon.co.jp" },
-            { name: "yodobashi.com", url: "https://www.yodobashi.com" },
-            { name: "TCB jeans",     url: "https://tcbjeans.stores.jp" },
-            { name: "BarnStormer",   url: "https://www.barnstormer.jp" },
-            { name: "HATCHET",       url: "https://hatchetmetalworkstudio.com" },
-        ],
-    },
+  {
+    title: "Daily",
+    links: [
+      { name: "Gmail",    url: "https://mail.google.com" },
+      { name: "Calendar", url: "https://calendar.google.com" },
+      { name: "GitHub",   url: "https://github.com" },
+    ],
+  },
+  {
+    title: "Media",
+    links: [
+      { name: "Youtube",     url: "https://youtube.com" },
+      { name: "Disney+",     url: "https://www.disneyplus.com" },
+      { name: "Prime Video", url: "https://www.amazon.co.jp/Amazon-Video/" },
+    ],
+  },
+  {
+    title: "Reddit",
+    links: [
+      { name: "r/commandline", url: "https://www.reddit.com/r/commandline" },
+      { name: "r/unixporn",    url: "https://www.reddit.com/r/unixporn" },
+    ],
+  },
+  {
+    title: "Shopping",
+    links: [
+      { name: "Amazon",        url: "https://amazon.co.jp" },
+      { name: "yodobashi.com", url: "https://www.yodobashi.com" },
+      { name: "TCB jeans",     url: "https://tcbjeans.stores.jp" },
+      { name: "BarnStormer",   url: "https://www.barnstormer.jp" },
+      { name: "HATCHET",       url: "https://hatchetmetalworkstudio.com" },
+    ],
+  },
 ];
